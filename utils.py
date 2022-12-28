cd5245ccfb1419ad856a02a2fb900c9d6300b1f5
0arta <51375114+0arta@users.noreply.github.com>
2022-12-28 09:40:29 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -19,8 +19,8 @@ def get_training_model():
     # The learner
     x = resnet_cifar10.learner(x, n_blocks)
 
-    # The Classifier for 10 classes
-    outputs = resnet_cifar10.classifier(x, 10)
+    # The Classifier for 100 classes
+    outputs = resnet_cifar10.classifier(x, 100)
 
     # Instantiate the Model
     model = tf.keras.Model(inputs, outputs)
